8084984616d620e833bb6299ba3fa289b76987c9
leostelon <53221136+leostelon@users.noreply.github.com>
2023-11-17 22:22:43 +0300

    create page without functions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles/navbar.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./screens/Home";
 import { Welcome } from "./screens/Welcome";
+import { Create } from './screens/Create';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/welcome" exact element={<Welcome />} />
+        <Route path="/create" exact element={<Create />} />
       </Routes>
     </Router>
   );
---
+++ src/screens/Create.jsx
@@ -0,0 +1,175 @@
+import "../styles/Home.css";
+import { Box, Button, IconButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { LeftDrawer } from "../components/LeftDrawer";
+import { toast } from "react-toastify";
+import { RiImageEditLine } from "react-icons/ri";
+import styled from "@emotion/styled";
+import MDEditor from "@uiw/react-md-editor";
+
+export const Create = () => {
+	const [title, setTitle] = useState("");
+	const [thumbnailFile, setThumbnailFile] = useState();
+	const [thumbnailFilePreview, setThumbnailFilePreview] = useState();
+	const [value, setValue] = useState("");
+
+	useEffect(() => {
+		if (thumbnailFile) {
+			const objectUrl = URL.createObjectURL(thumbnailFile);
+			setThumbnailFilePreview(objectUrl);
+
+			return () => URL.revokeObjectURL(objectUrl);
+		}
+	}, [thumbnailFile]);
+
+	return (
+		<Box
+			// sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+			sx={{ display: "flex" }}
+		>
+			<LeftDrawer />
+			<Box
+				style={{
+					// width: `80vw`,
+					// maxWidth: "1280px",
+					width: `calc(100vw - 280px)`,
+				}}
+			>
+				<Box
+					sx={{
+						p: 2,
+						display: "flex",
+						color: "#4c4848",
+					}}
+				>
+					<Box flex={3}>
+						<h2>Create</h2>
+						<br />
+						<Box>
+							{/* Image */}
+							<Box mb={2}>
+								{thumbnailFilePreview ? (
+									<Box
+										sx={{
+											width: "360px",
+											height: "240px",
+											borderRadius: "10px",
+
+											backgroundImage: `url(${thumbnailFilePreview})`,
+											backgroundRepeat: "no-repeat",
+											backgroundSize: "cover",
+											backgroundPosition: "center",
+
+											display: "flex",
+											justifyContent: "flex-end",
+
+											mt: 1,
+										}}
+									>
+										<IconButtonHolder
+											sx={{ m: 1 }}
+											component="label"
+											onChange={(e) => {
+												if (e.target.files[0]?.type?.split("/")[0] !== "image")
+													return toast(
+														"Please select a file with type image!",
+														{
+															type: "info",
+														}
+													);
+												if (e.target.files[0]?.type?.split("/")[1] !== "png")
+													return toast("Please only upload png images!", {
+														type: "info",
+													});
+												setThumbnailFile(e.target.files[0]);
+											}}
+										>
+											<RiImageEditLine />
+											<input type="file" hidden />
+										</IconButtonHolder>
+									</Box>
+								) : (
+									<Button
+										component="label"
+										onChange={(e) => {
+											console.log(e.target.files[0]?.type);
+											if (e.target.files[0]?.type?.split("/")[0] !== "image")
+												return toast("Please select a file with type image!", {
+													type: "info",
+												});
+											if (e.target.files[0]?.type?.split("/")[1] !== "png")
+												return toast("Please only upload png images!", {
+													type: "info",
+												});
+											setThumbnailFile(e.target.files[0]);
+										}}
+										sx={{ paddingLeft: "0px", width: "100%" }}
+									>
+										<input type="file" hidden />
+
+										<FileUploadContainer />
+									</Button>
+								)}
+							</Box>
+							{/* Title */}
+							<Box
+								className="param search-container"
+								sx={{ width: "100%" }}
+								mb={2}
+							>
+								<input
+									type="text"
+									id={`title`}
+									placeholder="Title"
+									value={title}
+									onInput={(e) => {
+										setTitle(e.target.value);
+									}}
+								/>
+							</Box>
+							<div data-color-mode="light">
+								<MDEditor value={value} onChange={setValue} />
+							</div>
+						</Box>
+					</Box>
+				</Box>
+			</Box>
+		</Box>
+	);
+};
+
+const IconButtonHolder = styled(IconButton)({
+	color: "white",
+	backgroundColor: "#191C22",
+	borderRadius: "5px",
+
+	width: "40px",
+	height: "40px",
+
+	marginRight: "8px",
+});
+
+const FileUploadContainer = (props) => {
+	return (
+		<Box
+			sx={{
+				border: "1.5px dashed darkgrey",
+				borderRadius: "10px",
+				height: "200px",
+
+				display: "flex",
+				flex: 1,
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+
+				bgcolor: "#c6c6c61c",
+				cursor: "pointer",
+			}}
+			{...props}
+		>
+			<h4 style={{ color: "darkgrey" }}>Cover Image</h4>
+			<input type="file" hidden />
+		</Box>
+	);
+};
